e12208e6fd9ce6053ecd1868395da35601a4e416
Nixon <voxell.technologies@gmail.com>
2023-11-05 23:01:20 +0800

    feat: use rect in hello_world

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,11 +1,10 @@
 use bevy::prelude::*;
-use bevy_log::LogPlugin;
 use bevy_vello_renderer::prelude::*;
 use vello::{kurbo, peniko, SceneBuilder, SceneFragment};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.build().disable::<LogPlugin>())
+        .add_plugins(DefaultPlugins)
         .add_plugins(VelloRenderPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, simple_animation)
@@ -21,7 +20,7 @@ fn setup(mut commands: Commands, mut fragments: ResMut<Assets<VelloFragment>>) {
         kurbo::Affine::default(),
         &peniko::Color::WHITE_SMOKE,
         None,
-        &kurbo::Circle::new((0.0, 0.0), 100.0),
+        &kurbo::RoundedRect::new(-50.0, -50.0, 50.0, 50.0, 10.0),
     );
 
     commands.spawn(Camera2dBundle::default());
@@ -38,11 +37,11 @@ fn simple_animation(
     mut q_transforms: Query<&mut Transform, With<Handle<VelloFragment>>>,
     time: Res<Time>,
 ) {
+    let sin_time: f32 = time.elapsed_seconds().sin().mul_add(0.5, 0.5);
+
     for mut transform in q_transforms.iter_mut() {
-        transform.scale = Vec3::lerp(
-            Vec3::ONE * 0.5,
-            Vec3::ONE * 1.0,
-            time.elapsed_seconds().sin().mul_add(0.5, 0.5),
-        );
+        transform.scale = Vec3::lerp(Vec3::ONE * 0.5, Vec3::ONE * 1.0, sin_time);
+        transform.translation = Vec3::lerp(Vec3::X * -100.0, Vec3::X * 100.0, sin_time);
+        transform.rotation = Quat::from_rotation_z(-std::f32::consts::TAU * sin_time);
     }
 }
